a3a4428a8764bf9abec903d5e56ad9ac17ff17a4
danadajian <danadajian@gmail.com>
2020-04-13 15:21:26 -0400

    * add credential variables

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,7 @@ git clone https://github.com/danadajian/danadajian.github.io.git
 cd danadajian.github.io
 cp -r ../build/. .
 
-git config user.email $GIT_EMAIL
-git config user.name $GIT_USERNAME
+git remote add origin "https://$GIT_USERNAME:$GIT_PASSWORD@github.com/danadajian/danadajian.github.io.git"
 git add .
 TIMESTAMP=$( date +"%Y-%m-%d_%H-%M-%S" )
 git commit -m "Circle CI build ${TIMESTAMP}"

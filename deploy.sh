b70a4196febc41f2d5457c637d2358065d19d322
danadajian <danadajian@gmail.com>
2020-04-13 15:26:34 -0400

    * edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ git clone https://github.com/danadajian/danadajian.github.io.git
 cd danadajian.github.io
 cp -r ../build/. .
 
-git remote add origin "https://$GIT_USERNAME:$GIT_PASSWORD@github.com/danadajian/danadajian.github.io.git"
+git remote set-url origin "https://$GIT_USERNAME:$GIT_PASSWORD@github.com/danadajian/danadajian.github.io.git"
 git add .
 TIMESTAMP=$( date +"%Y-%m-%d_%H-%M-%S" )
 git commit -m "Circle CI build ${TIMESTAMP}"

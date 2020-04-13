761f7bf20ba543ce639f6808af1b56b7db6d5d30
danadajian <danadajian@gmail.com>
2020-04-13 15:31:47 -0400

    * edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ git clone https://github.com/danadajian/danadajian.github.io.git
 cd danadajian.github.io
 cp -r ../build/. .
 
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_USERNAME"
 git remote set-url origin "https://$GIT_USERNAME:$GIT_PASSWORD@github.com/danadajian/danadajian.github.io.git"
 git add .
 TIMESTAMP=$( date +"%Y-%m-%d_%H-%M-%S" )

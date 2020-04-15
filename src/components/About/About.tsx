dceec1ed2ce7ddef2c8f80ff819741b32b804d4c
danadajian <danadajian@gmail.com>
2020-04-15 17:31:54 -0400

    * added dfs optimizer overview in projects * adjusted some css

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -15,54 +15,69 @@ export const About = (props: { selectedButton: string, changeSelectedButton: any
                 <Navbar selectedButton={selectedButton} changeSelectedButton={changeSelectedButton}/>
             </div>
             <div className="Container">
-                <body className="About">
-                <img src={headshot} style={{width: '400px', height: '400px'}} alt='Headshot'/>
-                <p>
-                    I am a self-taught programmer and have become adept at learning new technologies quickly. I love
-                    automating mundane tasks to save time. I'm a strong advocate of test-driven development and
-                    pair-programming, and I write software exclusively in <a href={CLEAN_CODE_LINK} target={"_blank"}
-                                                                             rel={"noopener noreferrer"}>clean code</a>.
-                </p>
-                <section>
-                    <h3>Professional Experience</h3>
-                    <p>
-                        <li>
-                            Hands-on experience using Node.js and the AWS serverless application model to
-                            productionalize machine learning models
-                        </li>
-                        <li>
-                            Experience refactoring and maintaining React.js and Python code
-                        </li>
-                    </p>
-                    <h3>Coursework</h3>
-                    <p>
-                        <li>Watched over 30 hours of course
-                            content on <a href={PLURALSIGHT_LINK} target={"_blank"}
-                                          rel={"noopener noreferrer"}>Pluralsight</a> spanning the topics of Java
-                            fundamentals, test-driven development, Java testing frameworks,
-                            object-oriented programming, and AWS frameworks
-                        </li>
-                        <li>
-                            Scored in the 88th percentile on Pluralsight's Java Fundamentals assessment.
-                        </li>
-                    </p>
-                </section>
-                <section>
-                    <h3>Education</h3>
-                    <p>
-                        <li>
-                            Graduated from Northwestern University with a Bachelor's Degree in Math and Economics.
-                        </li>
-                    </p>
-                </section>
-                <section>
-                    <h3>Interests</h3>
-                    <p>
-                        I play golf when it's over 40 degrees out, and paddle tennis otherwise. I also enjoy watching
-                        football, playing the piano, and producing music.
-                    </p>
-                </section>
-                </body>
+                <div className="About">
+                    <section>
+                        <body>
+                        <img src={headshot} style={{width: '400px', height: '400px'}} alt='Headshot'/>
+                        <p>
+                            I am a self-taught programmer and have become adept at learning new technologies quickly. I
+                            love
+                            automating mundane tasks to save time. I'm a strong advocate of test-driven development and
+                            pair-programming, and I write software exclusively in <a href={CLEAN_CODE_LINK}
+                                                                                     target={"_blank"}
+                                                                                     rel={"noopener noreferrer"}>clean
+                            code</a>.
+                        </p>
+                        </body>
+                    </section>
+                    <section>
+                        <body>
+                        <h3>Professional Experience</h3>
+                        <p>
+                            <li>
+                                Hands-on experience using Node.js and the AWS serverless application model to
+                                productionalize machine learning models
+                            </li>
+                            <li>
+                                Experience refactoring and maintaining React.js and Python code
+                            </li>
+                        </p>
+                        <h3>Coursework</h3>
+                        <p>
+                            <li>
+                                Watched over 30 hours of course
+                                content on <a href={PLURALSIGHT_LINK} target={"_blank"}
+                                              rel={"noopener noreferrer"}>Pluralsight</a> spanning the topics of Java
+                                fundamentals, test-driven development, Java testing frameworks,
+                                object-oriented programming, and AWS frameworks
+                            </li>
+                            <li>
+                                Scored in the 88th percentile on Pluralsight's Java Fundamentals assessment.
+                            </li>
+                        </p>
+                        </body>
+                    </section>
+                    <section>
+                        <body>
+                        <h3>Education</h3>
+                        <p>
+                            <li>
+                                Graduated from Northwestern University with a Bachelor's Degree in Math and Economics.
+                            </li>
+                        </p>
+                        </body>
+                    </section>
+                    <section>
+                        <body>
+                        <h3>Interests</h3>
+                        <p>
+                            I play golf when it's over 40 degrees out, and paddle tennis otherwise. I also enjoy
+                            watching
+                            football, playing the piano, and producing music.
+                        </p>
+                        </body>
+                    </section>
+                </div>
             </div>
             <Links/>
         </div>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -3,6 +3,8 @@ import './Projects.css';
 import {Navbar} from '../Navbar/Navbar';
 import {Links} from "../Links/Links";
 
+const dfsOptimizerExample = require('../../images/dfs-optimizer-example.png');
+
 export const Projects = (props: { selectedButton: string, changeSelectedButton: any }) => {
     const {selectedButton, changeSelectedButton} = props;
     return (
@@ -11,11 +13,54 @@ export const Projects = (props: { selectedButton: string, changeSelectedButton: 
                 <Navbar selectedButton={selectedButton} changeSelectedButton={changeSelectedButton}/>
             </div>
             <div className="Container">
-                <body className="Projects">
-                <p>
-                    Coming soon!
-                </p>
-                </body>
+                <div className="Projects">
+                    <section>
+                        <body>
+                        <h1>Daily Fantasy Sports Lineup Optimizer</h1>
+                        <a href={"https://dfsoptimizer.app"} target={"_blank"}
+                           rel={"noopener noreferrer"}>https://dfsoptimizer.app</a>
+                        <h3>Overview</h3>
+                        <p>
+                            My Daily Fantasy Sports (DFS) lineup optimizer aims to generate a lineup of players such
+                            that
+                            the lineup's total projected points are maximized, given the constraint that the lineup's
+                            total
+                            salary is within the salary cap. Each player has a position, a projection, and a salary, and
+                            a
+                            lineup must contain a certain number of each position. The optimization problem presented
+                            here
+                            is a type of <a href={"https://en.wikipedia.org/wiki/Knapsack_problem"}>knapsack problem</a>,
+                            which involves picking items with weights and values optimally to fit inside a knapsack.
+                        </p>
+                        <img src={dfsOptimizerExample} alt={'DFS Optimizer Example'}/>
+                        <h3>My Solution</h3>
+                        <p>
+                            The method I developed uses what I call "selective brute force", which reduces the problem
+                            efficiently to make checking all possibilities feasible. First, players are grouped by
+                            position
+                            and sorted by their salary-to-projection ratios. Then, players are removed from each
+                            position
+                            pool until the maximum number of lineup combinations is under a specified fixed threshold.
+                            Finally, a recursive algorithm efficiently iterates over possible lineups one by one,
+                            ensuring
+                            each new lineup it checks has the potential to be better (i.e. has a higher projected point
+                            total).
+                        </p>
+                        <p>
+                            The app currently supports Fanduel and Draftkings contests, and scrapes data from multiple
+                            sources.
+                        </p>
+                        </body>
+                    </section>
+                    <section>
+                        <body>
+                        <p>More project overviews coming soon! In the meantime, checkout my <a
+                            href={"https://github.com/danadajian/"} target={"_blank"}
+                            rel={"noopener noreferrer"}>GitHub</a>.
+                        </p>
+                        </body>
+                    </section>
+                </div>
             </div>
             <Links/>
         </div>
